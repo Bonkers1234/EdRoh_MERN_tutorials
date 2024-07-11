528e185e4c3f57a0d34b381263fe2eb608398c87
Bonkers <boroboro1@interia.pl>
2024-07-11 16:50:29 +0200

    Configured mongoose and created User model

--- FullstackResponsiveMERN/server/index.js
+++ FullstackResponsiveMERN/server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import { register } from './controllers/auth.js'
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url)
@@ -20,7 +21,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"))
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors)
+app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
 
 /* FILE STORAGE */
@@ -34,5 +35,13 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register)
 
-
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+  })
+  .catch((error) => console.log(`${error} did not connect`))
