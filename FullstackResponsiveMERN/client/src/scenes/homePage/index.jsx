a503a823e59d2e39418103c8151c92622d5a51a0
Bonkers <boroboro1@interia.pl>
2024-07-19 12:43:31 +0200

    Posts and Post Widgets part2

--- FullstackResponsiveMERN/client/src/scenes/homePage/index.jsx
+++ FullstackResponsiveMERN/client/src/scenes/homePage/index.jsx
@@ -3,6 +3,8 @@ import { Box, useMediaQuery } from "@mui/material"
 import { useSelector } from "react-redux"
 import Navbar from "scenes/navbar"
 import UserWidget from "scenes/widgets/UserWidget"
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import PostsWidget from "scenes/widgets/PostsWidget"
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width: 1000px)')
@@ -26,7 +28,8 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? '42%' : undefined}
           mt={isNonMobileScreens ? undefined : '2rem'}
         >
-
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={id} />
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis='26%'>
